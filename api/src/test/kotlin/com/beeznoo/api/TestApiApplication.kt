4dd5a70fbe6f509fff91dd8460c2bea458be7258
leonel_the13 <vicor32leonel@gmail.com>
2026-09-11 00:44:03 +0100

    chore (init-backend): inicializacao do projecto backend java, com numa estrutura de arquivos

--- api/src/test/kotlin/com/beeznoo/api/TestApiApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.beeznoo.api
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<ApiApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
